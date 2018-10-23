14f7675984b44827c51e1ef5b9276fb7a28f4698
Wade Jakahi <wjakahi@gmail.com>
2018-10-23 15:50:26 -0400

    adding dist

---
+++ precache-manifest.f20c971b2ff3dc1a7acd1f66a5ea7de0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/hello.world/robots.txt"
+  },
+  {
+    "revision": "6e17e92fcabb51eca839",
+    "url": "/hello.world/js/chunk-vendors.7d14b677.js"
+  },
+  {
+    "revision": "21495f3032fdf44c111d",
+    "url": "/hello.world/js/app.e7200a8d.js"
+  },
+  {
+    "revision": "3f7f9b8cd39f68f7bb00",
+    "url": "/hello.world/js/about.e33cf55b.js"
+  },
+  {
+    "revision": "77abc0d624f33e6ac217633b1eb4dead",
+    "url": "/hello.world/index.html"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/hello.world/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "6e17e92fcabb51eca839",
+    "url": "/hello.world/css/chunk-vendors.13951f06.css"
+  },
+  {
+    "revision": "21495f3032fdf44c111d",
+    "url": "/hello.world/css/app.919b9827.css"
+  }
+];
